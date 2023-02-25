dd0009872ac7494f4a5734f239c9ea553e0c1e98
Sebastian Mere <sebastian.mere@gmail.com>
2023-02-25 16:20:31 -0300

    Titles y metadata en todas las páginas

--- js/counter.js
+++ js/counter.js
@@ -1,29 +1,33 @@
 var animated = false;
-$(window).on('scroll', function() {
-  var sectionPosition = $('.counter').offset().top;
+$(window).on("scroll", function () {
+  var sectionPosition = $(".counter").offset().top;
   var screenPosition = $(window).scrollTop();
 
   if (screenPosition > sectionPosition - $(window).height() && !animated) {
-    $('.value, .valueError').each(function () {
+    $(".value, .valueError").each(function () {
       var value = $(this).text();
-      if ($(this).hasClass('valueError')) {
+      if ($(this).hasClass("valueError")) {
         value = 100;
-      }
-      else {
+      } else {
         value = 0;
       }
-      $(this).prop('Counter', value).animate({
-          Counter: $(this).text()
-      }, {
-          duration: 2000,
-          easing: 'swing',
-          step: function (now) {
-              $(this).text(Math.ceil(now));
+      $(this)
+        .prop("Counter", value)
+        .animate(
+          {
+            Counter: $(this).text(),
           },
-          complete: function() {
+          {
+            duration: 2000,
+            easing: "swing",
+            step: function (now) {
+              $(this).text(Math.ceil(now));
+            },
+            complete: function () {
               $(this).stop();
+            },
           }
-      });
+        );
     });
     animated = true;
   }

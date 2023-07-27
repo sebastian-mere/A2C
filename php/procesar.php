da3aaf133a6c6b3f985d9fe99e9b6b0112de2324
Sebastian Mere <sebastian.mere@gmail.com>
2023-07-27 17:58:05 -0300

    cambios en el PHP

--- php/procesar.php
+++ php/procesar.php
@@ -1,23 +1,49 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $nombre = $_POST["nombre"];
-  $empresa = $_POST["empresa"];
-  $email = $_POST["email"];
-  $telefono = $_POST["telefono"];
-  $pais = $_POST["pais"];
-  $interes = $_POST["interes"];
-  $comentarios = $_POST["comentarios"];
-  
-  $destinatarios = "rafaelrendon@any2cloud.com, sebastianalfaro@any2cloud.com";
-  $asunto = "Nuevo mensaje del formulario de contacto";
-  $mensaje = "Nombre y apellido: $nombre\nEmpresa: $empresa\nEmail: $email\nTeléfono: $telefono\nPaís: $pais\nInterés en: $interes\nComentarios: $comentarios";
-  
-  // Configurar cabeceras del correo
-  $cabeceras = "From: $email" . "\r\n" .
-               "Reply-To: $email" . "\r\n" .
-               "X-Mailer: PHP/" . phpversion();
-  
-  // Enviar el correo
-  mail($destinatarios, $asunto, $mensaje, $cabeceras);
+
+    # FIX: Replace this email with recipient email
+    $mail_to = "INGRESAR DIRECCION DE DESTINO";
+
+    # Sender Data
+    $name = str_replace(array("\r", "\n"), array(" ", " "), strip_tags(trim($_POST["nombre"])));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $message = trim($_POST["comentarios"]);
+    $subject = "Mensaje de $name desde el sitio web";
+
+    if (empty($name) || !filter_var($email, FILTER_VALIDATE_EMAIL) || empty($message)) {
+        # Set a 400 (bad request) response code and exit.
+        http_response_code(400);
+        echo "Please complete the form and try again.";
+        exit;
+    }
+
+    # Mail Content
+    $content = "Nombre: $name\n";
+    $content .= "Email: $email\n\n";
+    $content .= "Mensaje:\n$message\n";
+
+    # email headers.
+    $headers = "From: INGRESAR DIRECCION DE DESTINO\nReply-To:" . $email . "\n";
+    $headers .= "Mime-Version: 1.0\n";
+    $headers .= "Content-Type: text/plain";
+
+    # Send the email.
+    $success = mail($mail_to, $subject, $content, $headers);
+    if ($success) {
+        # Set a 200 (okay) response code.
+        http_response_code(200);
+        header("Location: http://electrocasac.com.ar/index.html#success");
+        /*echo "Thank You! Your message has been sent.";*/
+    } else {
+        # Set a 500 (internal server error) response code.
+        http_response_code(500);
+        echo "Oops! Something went wrong, we couldn't send your message.";
+    }
+
+} else {
+    # Not a POST request, set a 403 (forbidden) response code.
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
 }
 ?>
+

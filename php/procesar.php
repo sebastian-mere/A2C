5045e33c857c2a655e223aa53117d898ade67b18
Sebastian Mere <sebastian.mere@gmail.com>
2023-08-06 16:52:19 -0300

    correcciones en PHP y about

--- php/procesar.php
+++ php/procesar.php
@@ -2,11 +2,7 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     # FIX: Replace this email with recipient email
-<<<<<<< HEAD
     $mail_to = "Leads@any2cloud.com";
-=======
-    $mail_to = "INGRESAR DIRECCION DE DESTINO";
->>>>>>> 7d89d36f129b3afff4dc75c4a0772b46dcc4eb5a
 
     # Sender Data
     $name = str_replace(array("\r", "\n"), array(" ", " "), strip_tags(trim($_POST["nombre"])));
@@ -27,11 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $content .= "Mensaje:\n$message\n";
 
     # email headers.
-<<<<<<< HEAD
     $headers = "From: Leads@any2cloud.com\nReply-To:" . $email . "\n";
-=======
-    $headers = "From: INGRESAR DIRECCION DE DESTINO\nReply-To:" . $email . "\n";
->>>>>>> 7d89d36f129b3afff4dc75c4a0772b46dcc4eb5a
     $headers .= "Mime-Version: 1.0\n";
     $headers .= "Content-Type: text/plain";
 
@@ -40,11 +32,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($success) {
         # Set a 200 (okay) response code.
         http_response_code(200);
-<<<<<<< HEAD
         header("Location: #success");
-=======
-        header("Location: http://electrocasac.com.ar/index.html#success");
->>>>>>> 7d89d36f129b3afff4dc75c4a0772b46dcc4eb5a
         /*echo "Thank You! Your message has been sent.";*/
     } else {
         # Set a 500 (internal server error) response code.
